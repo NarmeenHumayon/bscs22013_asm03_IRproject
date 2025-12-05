39632230b288c70a80269827d3c29dee006eb6b4
NarmeenHumayon <128923359+NarmeenHumayon@users.noreply.github.com>
2025-12-06 00:49:50 +0500

    Update positional_index.py

--- src/positional_index.py
+++ src/positional_index.py
@@ -1,19 +1,18 @@
 # src/positional_index.py
+
 import pickle
 import json
 import os
 
-# Paths
 DOCS_FILE = "data/docs.pkl"
 OUTPUT_PKL = "models/positional_index.pkl"
 OUTPUT_JSON = "models_json/positional_index.json"
 
 
 def build_positional_index(docs):
-    """
-    Build a positional inverted index:
-    term -> { doc_id: [positions] }
-    """
+    # concept: term -> { doc_id: [positions] }
+    #this is how appears in json, pkl format we cant read
+
     index = {}
     for doc_id, tokens in docs.items():
         for pos, term in enumerate(tokens):
@@ -25,27 +24,25 @@ def build_positional_index(docs):
     return index
 
 def save_index(index, pkl_file=OUTPUT_PKL, json_file=OUTPUT_JSON):
-    # Ensure JSON folder exists
+    # json directory: models_json/positional_index.py
     os.makedirs(os.path.dirname(json_file), exist_ok=True)
 
-    # Save as pickle
+    # pkl
     with open(pkl_file, "wb") as f:
         pickle.dump(index, f)
     
-    # Save as JSON (for human readability)
+    #json
     with open(json_file, "w", encoding="utf-8") as f:
         json.dump(index, f, ensure_ascii=False, indent=2)
 
-    print(f"Positional index saved as pickle: {pkl_file}")
-    print(f"Positional index saved as JSON: {json_file}")
+    print(f"Positional_index is saved as pickle: {pkl_file}")
+    print(f"Positional_index  is saved as JSON: {json_file}")
 
 if __name__ == "__main__":
-    # Load preprocessed docs
     with open(DOCS_FILE, "rb") as f:
         docs = pickle.load(f)
 
-    # Build index
     positional_index = build_positional_index(docs)
 
-    # Save index
     save_index(positional_index)
+
